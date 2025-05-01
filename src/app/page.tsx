258efd6613d70e61cb1665c98f7b5996842464ee
Jose <josegabrielvilchezc@gmail.com>
2025-04-30 23:50:08 -0400

    rm comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import DashboardDisplay from "@/components/DashboardDisplay";
-// Removed api imports: import { getLeaderboardData, getMarketData } from "@/utils/api";
 import pageStyles from "@/styles/pages/HomePage.module.scss";
 
 export default async function Home() {
